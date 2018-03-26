1e27bde1fe331366965e6c1d27839fc8ba4e2726
Felix <fsellmann@gmx.de>
2018-03-26 22:12:53 +0200

    Update twist_controller.py
    I implemented the yaw-controller component.
    Moreover I added an other option of deceleration-calculation.
    Finally I adjusted the P- and I-values of the PID-controller a bit, it seemed to be a little big.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,5 +1,6 @@
 from lowpass import LowPassFilter
 from pid import PID
+from yaw_controller import YawController
 import numpy as np
 
 GAS_DENSITY = 2.858
@@ -17,8 +18,12 @@ class Controller(object):
         self.wheel_base = args[5]
         self.steer_ratio = args[6]
         self.max_steer_angle = args[7]
+        self.max_lat_accel = args[8]
         self.lowpass = LowPassFilter(self.accel_limit, self.delta_t)
-        self.pid = PID(2.0, 0.4, 0.1, mn=-0.2, mx=0.2)
+        #self.pid = PID(2.0, 0.4, 0.1, mn=-0.2, mx=0.2)
+        self.pid = PID(1.2, .2, 0.1, mn=args[2], mx=args[3])        
+        self.steering_controller = YawController(args[5], args[6], 0.0, args[8], args[7])
+
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
@@ -33,11 +38,15 @@ class Controller(object):
         steer = 0.
         
         if dbw_enabled:
-            steer = target_angular_velocity * self.steer_ratio
+            #steer = target_angular_velocity * self.steer_ratio
+            steer = self.steering_controller.get_steering(target_linear_velocity, target_angular_velocity, 
+                                                          current_linear_velocity)
             velocity_error = target_linear_velocity - current_linear_velocity
-            throttle = self.pid.step(velocity_error, self.delta_t)
+            throttle = self.pid.step(velocity_error, self.delta_t) # [m/s²]
             if throttle < 0.:
-                brake = -throttle
+                #brake = -throttle
+                # maybe its better to calculate the deceleration as "brake torque"
+                brake = self.vehicle_mass * abs(throttle) * self.wheel_radius # [Nm]
                 throttle = 0.
         else:
             self.pid.reset()
